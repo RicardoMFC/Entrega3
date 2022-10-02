1fa0d3c24c705394b5d4c8fa3cd181a966c377ae
RicardoMFC <113980950+RicardoMFC@users.noreply.github.com>
2022-10-02 19:22:15 +0000

    t

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,5 +1,8 @@
-peso = input ("Introduzca su peso en kilogramos\n")
-altura = input ("Introduzca su estatura en cm\n")
+peso = float( input ("Introduzca su peso en kilogramos\n"))
+altura = float (input ("Introduzca su estatura en metros\n"))
 
-print ('El peso es de {0} y la estatura es de {1}'.format(peso, altura))
-print ('La altura es de {1} y el peso es de {0}'.format(peso, altura))
+print ('El peso es de {0} KG y la estatura es de {1} metros'.format(peso, altura))
+print ('La altura es de {1} metros y el peso es de {0} KG'.format(peso, altura))
+#Otra forma
+print ('El peso es de {} KG y la estatura es de {} metros'.format(peso, altura))
+print ('La altura es de {} metros y el peso es de {} KG'.format(altura, peso))
